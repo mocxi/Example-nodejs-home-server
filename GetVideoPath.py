b371e74dc4027b1e7886425a96416b302bbf2b52
mocxi <l.h.huy1612@gmail.com>
2016-05-28 19:19:00 +0700

    *fix bug run server on linux

--- GetVideoPath.py
+++ GetVideoPath.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python
 import os, json
 FileList = []
 FileListJson = {}
 FileListJson['video'] = []
-print json.dumps(FileListJson)
+print(json.dumps(FileListJson))
 oldpath = path = os.getcwd()
-print path
+print(path)
 
 import glob
 extList = ['.mp4','.avi']
 # FileList = glob.glob('./*.js')
 for root, dirs, files in os.walk('./'):
-    for file in files:
+	for file in files:
 		for ext in extList:
 			if file.endswith(ext):
 				jsonObj = {}
@@ -25,6 +26,6 @@ else:
 	for f in FileList:
 		print(f)
 os.chdir(oldpath)
-print json.dumps(FileListJson)
+print(json.dumps(FileListJson))
 with open('./config/VidList.json','w+') as outFile:
 	outFile.write(json.dumps(FileListJson))
